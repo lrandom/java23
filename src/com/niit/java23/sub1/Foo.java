8084643a070345c1600232515f74e426d4c265c6
Nguyen Thanh Luan <luann4099@gmail.com>
2022-02-17 21:20:49 +0700

    demo

--- src/com/niit/java23/sub1/Foo.java
+++ src/com/niit/java23/sub1/Foo.java
@@ -1,10 +1,12 @@
 package com.niit.java23.sub1;
 
 import java.util.ArrayList;
+import java.util.StringTokenizer;
 
 public class Foo {
     public static void main(String[] args) {
+       String name = "John Smith";
+       name.compareToIgnoreCase()
 
     }
 }
-args
