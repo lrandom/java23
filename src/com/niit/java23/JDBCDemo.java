4187943867e23bd5e34315f698e848705033244c
Nguyen Thanh Luan <luann4099@gmail.com>
2022-02-24 20:24:17 +0700

    update code

--- src/com/niit/java23/JDBCDemo.java
+++ src/com/niit/java23/JDBCDemo.java
@@ -12,7 +12,7 @@ public class JDBCDemo {
 
             //thực thi các câu lệnh sql
             //thực thi câu lệnh insert
-            Statement stm = conn.createStatement();
+/*            Statement stm = conn.createStatement();
             stm.executeUpdate("INSERT INTO users(email,pwd,salary) VALUES ('mm@gmail.com','1234',1000)");
 
             ResultSet rs = stm.executeQuery("SELECT * FROM users");
@@ -21,7 +21,32 @@ public class JDBCDemo {
                 Double salary = rs.getDouble("salary");
                 System.out.println(email);
                 System.out.println(salary);
-            };
+            };*/
+
+
+            //tạo ra result set mà có khả năng update dữ liệu
+            Statement stm = conn.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_UPDATABLE);
+            ResultSet rs = stm.executeQuery("SELECT * FROM users");
+            //rs.relative(3);
+            /*rs.absolute(2);
+            rs.updateString("email", "bumblebee@gmail.com");
+            rs.updateRow();*/
+
+
+            //chèn thêm bản ghi vào
+            rs.moveToInsertRow();
+            rs.updateString("email", "a@gmail.com");
+            rs.updateString("pwd", "234");
+            rs.updateFloat("salary", new Float(1000));
+            rs.insertRow();
+
+ /*           do {
+                String email = rs.getString("email");
+                Double salary = rs.getDouble("salary");
+                System.out.println(email);
+                System.out.println(salary);
+            } while (rs.next());*/
+
 
         } catch (Exception e) {
             e.printStackTrace();
