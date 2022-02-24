21ab4daf8c5f7c9ff7f0b90db62eef16cd19a6f6
Nguyen Thanh Luan <luann4099@gmail.com>
2022-02-24 19:47:07 +0700

    update jdbc demo

--- src/com/niit/java23/JDBCDemo.java
+++ src/com/niit/java23/JDBCDemo.java
@@ -1,15 +1,28 @@
 package com.niit.java23;
 
-import java.sql.Driver;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class JDBCDemo {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver"); //load driver
             //Kết nối đến mysql server
-            DriverManager.getConnection("jdbc:mysql://localhost:3306/javaweb_23",
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/javaweb_23",
                     "root", "koodinh@");
+
+            //thực thi các câu lệnh sql
+            //thực thi câu lệnh insert
+            Statement stm = conn.createStatement();
+            stm.executeUpdate("INSERT INTO users(email,pwd,salary) VALUES ('mm@gmail.com','1234',1000)");
+
+            ResultSet rs = stm.executeQuery("SELECT * FROM users");
+            while(rs.next()) {
+                String email = rs.getString("email");
+                Double salary = rs.getDouble("salary");
+                System.out.println(email);
+                System.out.println(salary);
+            };
+
         } catch (Exception e) {
             e.printStackTrace();
         }
